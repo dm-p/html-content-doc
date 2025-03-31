04876c6242b3ea70ffe38e8ef40c5a05167d2133
Daniel Marsh-Patrick <daniel@coacervo.co>
2025-03-31 14:01:28 +1300

    docs: change baseUrl (as GH pages coexist in my personal repo)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   url: "https://html-content.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/html-content-doc",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
